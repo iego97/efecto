e6ee316e37587fa8b77d0717fc44ebb6f6663d83
iram_ <iram_@DESKTOP-FQDQHKI>
2018-03-01 10:42:07 -0700

    Agregar archivos de proyecto.

---
+++ Efectos/Efecto.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.Wave;
+
+namespace Efectos
+{
+    class Efecto : ISampleProvider
+    {
+        private ISampleProvider fuente;
+
+        public Efecto(ISampleProvider fuente)
+        {
+            this.fuente = fuente;
+        }
+
+        public WaveFormat WaveFormat
+        {
+            get
+            {
+                return fuente.WaveFormat;
+            }
+        }
+
+        public int Read(float[] buffer, int offset, int count)
+        {
+            var read = fuente.Read(buffer, offset, count);
+
+            for (int i = 0; i < read; i++)
+            {
+
+                //Efecto
+                buffer[offset + i] *= 0.56f;
+            }
+
+
+            return read;
+        }
+
+
+    }
+}
---
+++ Efectos/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Microsoft.Win32;
+
+using NAudio.Wave;
+
+
+namespace Efectos
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        WaveOutEvent waveOut;
+        AudioFileReader reader;
+        Efecto efectoProvider;
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            waveOut = new WaveOutEvent();
+        }
+
+        private void btnExaminar_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            if((bool)fileDialog.ShowDialog())
+            {
+                txtRuta.Text = fileDialog.FileName;
+                reader = new AudioFileReader(fileDialog.FileName);
+            }
+                
+
+                    }
+
+        private void btnPlay_Click(object sender, RoutedEventArgs e)
+        {
+            if(waveOut != null)
+            {
+                waveOut.Init(reader);
+                waveOut.Play();
+            }
+                  
+        }
+
+        private void btnEfecto_Click(object sender, RoutedEventArgs e)
+        {
+            if (waveOut != null && reader != null)
+            {
+
+                if(waveOut.PlaybackState == PlaybackState.Playing)
+                {
+                    waveOut.Stop();
+                }
+
+                waveOut.Init(reader);
+
+                efectoProvider = new Efecto(reader);
+                waveOut.Init(efectoProvider);
+
+
+                waveOut.Play();
+            }
+        }
+    }
+}
